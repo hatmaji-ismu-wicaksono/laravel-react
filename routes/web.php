fc10e7773059fdfa80fdcc5c160b3253c4ba59f2
hatmaji_ismu <hatmaji.ismu@gmail.com>
2023-02-26 21:32:21 +0700

    adding laravel react and laravel auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => true, 'reset' => false, 'verify' => false]);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
